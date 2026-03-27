5bee57928f95720eb762599897feeeea6d268ddf
ProjectCaffeine <smith_eg21@yahoo.com>
2026-03-27 16:57:43 -0500

    Added embedded file

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,24 @@
 const std = @import("std");
-const inzersion = @import("inzersion");
+const json_data = @embedFile("testconflictversioninfo.json");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try inzersion.bufferedPrint();
+    //var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+    //const allocator = gpa.allocator();
+
+    std.debug.print("File:\n{s}", .{json_data[0..64]});
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+fn readFile(allocator: std.mem.Allocator) void {
+    const file = std.fs.cwd().openFile("testconflictversioninfo.json", .{}) catch |err| {
+        std.log.err("Failed to open file: {s}", .{@errorName(err)});
+        return;
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+    defer file.close();
+
+    std.debug.print("File read!", .{});
+
+    const content = try file.readToEndAlloc(allocator, 128);
+
+    _ = content;
 }
